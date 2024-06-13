fc4a3064289093929bba3e29c2ad660b2c0b9eee
kamandula <kamandula@gmail.com>
2024-06-13 17:04:33 +0000

    Task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,13 +10,13 @@ public_users.post("/register", (req,res) => {
     return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get the book list available in the shop
+// Get the book list available in the shop - Task 1
 public_users.get('/',function (req, res) {
     // Get the JSON of books
     return res.status(200).send(JSON.stringify({books}, null, 4));
 });
 
-// Get book details based on ISBN
+// Get book details based on ISBN - Task 2
 public_users.get('/isbn/:isbn',function (req, res) {
     // Retrieve the ISBN from the request parameters
     const isbn = req.params.isbn;
@@ -36,7 +36,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
     return res.status(404).json({message: "Book not found"});
  });
   
-// Get book details based on author
+// Get book details based on author - Task 3
 public_users.get('/author/:author',function (req, res) {
     // Retrieve the author from the request parameters
     const author = req.params.author;
@@ -57,10 +57,25 @@ public_users.get('/author/:author',function (req, res) {
     return res.status(404).json({message: "Book not found"});
 });
 
-// Get all books based on title
+// Get all books based on title - Task 4
 public_users.get('/title/:title',function (req, res) {
-    //Write your code here
-    return res.status(300).json({message: "Yet to be implemented"});
+    // Retrieve the title from the request parameters
+    const title = req.params.title;
+    // Initialize an empty array to store the books with the given title
+    let booksByTitle = [];
+    // Iterate through the ‘books’ array & check the title matches the one
+    for(let book in books){
+        if(books[book].title == title){
+            booksByTitle.push(books[book]);
+        }
+    };
+    // Check if any books were found with the given title
+    if(booksByTitle.length > 0){
+        // Return the array of books with the given title
+        return res.status(200).send(JSON.stringify(booksByTitle, null, 4));
+    }
+    // If no books were found, return a 404 error
+    return res.status(404).json({message: "Book not found"});
 });
 
 //  Get book review
