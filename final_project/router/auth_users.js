ee43eb3f7e32fa05712a02be21820acbf7f3a012
kamandula <kamandula@gmail.com>
2024-06-13 18:14:03 +0000

    Task 8

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -49,10 +49,52 @@ regd_users.post("/login", (req,res) => {
 
 });
 
-// Add a book review
+// Add a book review - Task 8
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Authentication is already done by the middleware
+  // Retrieve the ISBN from the request parameters
+  const isbn = req.params.isbn;
+  // Retrieve the review from the request query
+  const review = req.query.review;
+  // Retrieve the username from the session
+  const username = req.user.username;
+  // Initialize a boolean variable to check if the book is found
+  let bookFound = false;
+  // Initialize a boolean variable to check if the book is reviewed by the user
+  let userReviewed = false;
+  // Iterate through the ‘books’ array & check the ISBN matches the one
+  for(let book in books){
+    if(books[book].isbn == isbn){
+      // Check if the user has already reviewed the book
+      for(let review_user in books[book].reviews){
+        if(books[book].reviews[review_user].username == username){
+          // Modify the existing review
+          books[book].reviews[review_user].review = review;
+          bookFound = true;
+          userReviewed = true;
+          break;
+        }
+      }
+      // If the user has not reviewed the book, add a new review
+      if(!bookFound){
+        books[book].reviews[username] = {username, review};
+        bookFound = true;
+      }
+      break;
+    }
+  };
+  console.log({books});
+  // Check if the book is found
+  if(bookFound){
+    // Return a success message
+    if (userReviewed){
+      return res.status(200).json({message: "Review modified successfully"});
+    }
+    return res.status(200).json({message: "Review added successfully"});
+  } else {
+    // If the book is not found, return a 404 error
+    return res.status(404).json({message: "Book not found"});
+  };
 });
 
 module.exports.authenticated = regd_users;
