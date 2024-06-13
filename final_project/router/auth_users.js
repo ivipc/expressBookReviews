b773857dbe5fb2f64a5acc61ed1438880907a945
kamandula <kamandula@gmail.com>
2024-06-13 17:55:42 +0000

    Task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,8 +5,8 @@ const regd_users = express.Router();
 
 let users = [];
 
+// Check if the username already exists - Task 6
 const isValid = (username)=>{ // returns boolean
-  // Check if the username already exists
   for(let user in users){
     if(users[user].username == username){
       return true;
@@ -19,10 +19,37 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
 }
 
-//only registered users can login
+// only registered users can login - Task 7
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Logging in as a registered user
+  const {username, password} = req.body;
+  // Check if the username and password are provided
+  if(!username || !password){
+    return res.status(400).json({message: "Username and password are required"});
+  }
+  // Check if the username is already registered
+  if(!isValid(username)){
+    return res.status(401).json({message: "Invalid credentials"});
+  }
+  // Define a boolean variable to check if the password is correct
+  let isPasswordCorrect = false;
+  // Check if the password is correct
+  for(let user in users){
+    if(users[user].username == username && users[user].password == Buffer.from(password).toString('base64')){
+      isPasswordCorrect = true;
+      break;
+    }
+  };
+  if(!isPasswordCorrect){
+    return res.status(401).json({message: "Invalid credentials"});
+  }
+  // Generate token and save it in the session
+  const token = jwt.sign({username}, "token_customer", {expiresIn: "1h"});
+  req.session.token = token;
+  // Return a success message
+  return res.status(200).json({message: "Login successful"});
+  
+
 });
 
 // Add a book review
