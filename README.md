a377c1fb3e01c969ee66442b69c0b1a183831cd9
kamandula <kamandula@gmail.com>
2024-06-13 16:59:49 +0000

    Task 3

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # coding-project-template
+
+## Laboratorio práctico: Aplicación de revisión de librosEstado
+
+En este proyecto final, construiremos una aplicación de revisión de libros en línea del lado del servidor y la integraremos con un servidor REST API seguro que utilizará autenticación a nivel de sesión mediante JWT. A continuación, probará su aplicación utilizando callbacks Promises o funciones Async-Await.
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,7 +22,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
     // Find the book with the given ISBN
     for(let book in books){
-        if(books[book].isbn==isbn){
+        if(books[book].isbn == isbn){
             bookFound = books[book];
             break;
         }
@@ -38,8 +38,23 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    //Write your code here
-    return res.status(300).json({message: "Yet to be implemented"});
+    // Retrieve the author from the request parameters
+    const author = req.params.author;
+    // Initialize an empty array to store the books by the given author
+    let booksByAuthor = [];
+    // Iterate through the ‘books’ array & check the author matches the one provided in the request parameters
+    for(let book in books){
+        if(books[book].author == author){
+            booksByAuthor.push(books[book]);
+        }
+    };
+    // Check if any books were found by the given author
+    if(booksByAuthor.length > 0){
+        // Return the array of books by the given author
+        return res.status(200).send(JSON.stringify(booksByAuthor, null, 4));
+    }
+    // If no books were found, return a 404 error
+    return res.status(404).json({message: "Book not found"});
 });
 
 // Get all books based on title
